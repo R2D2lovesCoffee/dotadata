009c5a9776ea6272192827e4a47a17bfe542edc1
cosmin.petre@aquasoft.ro <petre.cosmin1997@gmail.com>
2020-05-03 16:15:42 +0300

    auth middleware. login route

--- server/router.js
+++ server/router.js
@@ -10,6 +10,38 @@ const { randomNumber } = require('./scripts/utils');
 const Question = require('./scripts/questions/Question');
 const User = require('./database/models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {secret } =require('./config');
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if(email && password) {
+        User.findOne({where: {email}, raw: true})
+        .then(user => {
+            if(user) {
+                bcrypt.compare(password, user.password_hash)
+                .then(result => {
+                    if(result) {
+                        const token = jwt.sign({user_id: user.id});
+                        res.send(token);
+                    } else {
+                        res.status(400).send({message: 'Email or password is incorrect!'});        
+                    }
+                }).catch(err => {
+                    console.log(err);
+                    res.status(500).send({message: 'Something went wrong. Please try again later.'});
+                })
+            } else {
+                res.status(400).send({message: 'Email or password is incorrect!'});
+            }
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send({message: 'Something went wrong. Please try again later.'});
+        })
+    } else {
+        res.status(400).send({message: 'Wrong body format!'});
+    }
+})
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
