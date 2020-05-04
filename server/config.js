1ecc8829721c45af92745856ffaa52126ca54a40
cosmin.petre@aquasoft.ro <petre.cosmin1997@gmail.com>
2020-05-04 11:23:50 +0300

    fixed config

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const config = {};
 
 let lines = [];
 try {
-    lines = fs.readFileSync('./.env', 'utf8').split('\n');
+    lines = fs.readFileSync('./.env', 'utf8').replace(/\r/g, '').split('\n');
 } catch (err) {
     console.log('The .env file doesn\'t exist. Please create the file according to example.env file.');
     console.log(err);
@@ -17,24 +17,9 @@ lines.forEach(line => {
 
 const keys = Object.keys(config);
 
-const allKeysSet = ['DBHOST', 'PORT', 'DBDIALECT', 'DBNAME', 'DBUSER', 'DBPASS'].every(key => keys.indexOf(key) >= 0);
-
+const allKeysSet = ['DBHOST', 'PORT', 'DBDIALECT', 'DBNAME', 'DBUSER', 'DBPASS', 'SECRET'].every(key => keys.indexOf(key) >= 0);
 if (!allKeysSet) {
     console.log('The .env file requires specific keys. You can find them in example.env file.');
     process.exit();
 }
 module.exports = config;
-// cum ar zice wd. 'wait for it!'
-// module.exports = {
-//     DBHOST:'localhost',
-//     PORT:5000,
-//     DBDIALECT:'mysql',
-//     DBNAME: 'dota_data',
-//     DBUSER:'root',
-//     DBPASS: 'baschetnba68'
-// };
-//stai asa sa iti fac user la proiect sa poti da push ah. ai cont de github? fa-ti repede unul. www.github.com inchide ochii cat timp fac sa nu mi furi datecu le.
-//cu config.js sa nu dai push niciodata. 
-//practic tot codul din fisierul asta e inutil la tine. dintr-un annume motiv. asa ca am scris obiectul ala de mana
-//ciudat totusi..
-// arata-mi baza de date
