46b02119990daf1b884bbcefcbd44a77f40b2578
cosmin.petre@aquasoft.ro <petre.cosmin1997@gmail.com>
2020-05-05 10:51:30 +0300

    app name

--- server/app.js
+++ server/app.js
@@ -9,9 +9,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use('/api', router);
 
-app.use(express.static(path.join(__dirname, '../dota-data/build')));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../dota-data/build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../dota-data/build')));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../dota-data/build', 'index.html'));
+// });
 
 module.exports = app;
