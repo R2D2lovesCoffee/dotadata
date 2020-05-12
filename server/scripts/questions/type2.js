c5be6ec6e0c022d9f68ead45346df7e9ddeb00c9
BackinR2D2 <cristimlg334@gmail.com>
2020-05-12 11:13:29 +0300

    types

--- server/scripts/questions/type2.js
+++ server/scripts/questions/type2.js
@@ -1,3 +1,29 @@
 const Question = require('./Question');
-const question = new Question(2);
-process.send({ message: 'hello from type2' });
+const Hero = require('../../database/models/Hero');
+const HeroDetails = require('../../database/models/HeroDetail');
+const db = require('../../database/connection');
+const { randomNumber } = require('../utils');
+require('../../database/associations')();
+
+const question = new Question();
+
+question.meta = {
+    subjectType: 'img',
+    answerType: 'text'
+}
+
+question.setText("who is this hero?");
+
+Hero.findAll({
+    attributes: ['img_src'],
+    order: [
+        db.fn('RAND')
+    ],
+    limit: 4,
+    include: [
+        { model: HeroDetails, as: heroes, attributes: ['src'], required: true }
+    ]
+})
+
+
+// process.send({ message: 'hello from type2' });
