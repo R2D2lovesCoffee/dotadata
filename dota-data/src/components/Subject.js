cdefd6daad302fdf131dcd47ac24f5b99bd3bf5b
cosmin.petre@aquasoft.ro <petre.cosmin1997@gmail.com>
2020-06-12 12:26:09 +0300

    fixed audio not changing issue

--- dota-data/src/components/Subject.js
+++ dota-data/src/components/Subject.js
@@ -2,42 +2,31 @@ import React, { useState, useEffect, useRef } from 'react';
 import '../components/views/Home.css';
 
 export default function Subject(props) {
-    const [content, setContent] = useState('');
-    const [type, setType] = useState('');
     const audio = useRef(null);
     useEffect(() => {
-        setContent(props.content);
-    }, [props.content]);
-    useEffect(() => {
-        setType(props.type);
-    }, [props.type]);
-    useEffect(() => {
-        if (type === 'audio' && content) {
+        if (props.type === 'audio' && props.content) {
+            audio.current.pause();
+            audio.current.load();
             audio.current.play();
         }
-    }, [content]);
-    useEffect(() => {
-        if (type === 'audio' && content) {
-            audio.current.play();
-        }
-    }, [type]);
-    switch (type) {
+    }, [props.type, props.content])
+    switch (props.type) {
         case 'audio':
             return (
                 <div className="container" id='special'>
                     <audio controls className="container embed-responsive-item" ref={audio}>
-                        <source src={content} type="audio/mpeg" />
+                        <source src={props.content} type="audio/mpeg" />
                         Your browser does not support the audio element.
                     </audio>
                 </div>
             )
         case 'img':
             return (<div className="container" id='special'>
-                <img className='centerImage' src={content} />
+                <img className='centerImage' src={props.content} />
             </div>)
         case 'text':
             return (<div className="container" id='special'>
-                {content}
+                {props.content}
             </div>)
         case 'none':
             return (<div></div>)
