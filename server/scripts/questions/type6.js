159e64c0369e6086fff48b8087c0acf64dc4e0fe
BackinR2D2 <cristimlg334@gmail.com>
2020-06-16 17:51:43 +0300

    type6js

--- server/scripts/questions/type6.js
+++ server/scripts/questions/type6.js
@@ -12,7 +12,7 @@ question.meta = {
     answersType: 'text'
 }
 
-question.setText('Is this hero ranged or melee?');
+question.setText('Is this hero melee or ranged?');
 
 Hero.findAll({
     attributes: ['range', 'img_medium_src'],
@@ -22,19 +22,27 @@ Hero.findAll({
     limit: 2
 }).then(heroes => heroes.map(hero => hero.dataValues))
     .then(heroes => {
-        const heroRange = ['Melee', 'Ranged'];
-        question.setAnswers(heroRange);
-        if (heroRange !== heroes.map(hero => hero.range)) {
-            heroes.map(hero => hero.range).splice(0, heroes.map(hero => hero.range).length, ...heroRange);
-            //var correct = heroRange.indexOf('melee') === heroes.map(hero => hero.range).indexOf('melee') ? 0 : 1;
-            //var correct = heroRange.includes('melee') === heroes.map(hero => hero.range).includes('melee') ? 0 : 1;
-            if (heroRange[0] === heroes.map(hero => hero.range)[0]) {
-                var correct = 0;
+        const heroRange = ['melee', 'ranged'];
+
+        heroes.map(hero => hero.range)[0] = 'melee';
+        heroes.map(hero => hero.range)[1] = 'ranged';
+        let correct = null;
+
+        if (heroes.map(hero => hero.range)[0] === heroRange[0]) {
+            correct = 0;
+        }
+        if (heroes.map(hero => hero.range)[1] === heroRange[1]) {
+            correct = 1;
+        }
+        if (correct === null) {
+            if (heroes.map(hero => hero.range)[0] === heroRange[0]) {
+                correct = 0;
             }
-            if (heroRange[1] === heroes.map(hero => hero.range)[1]) {
-                var correct = 1;
+            if (heroes.map(hero => hero.range)[1] === heroRange[1]) {
+                correct = 1;
             }
         }
+        question.setAnswers(heroRange.map(heroR => heroR.charAt(0).toUpperCase() + heroR.slice(1)));
         question.setSubject(heroes[correct].img_medium_src);
         process.send({ question, correct });
     })
